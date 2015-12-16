c7ca76e62fad95102542b0b00336d35fd14ce237
Murphy Stroppa <Murph33@gmail.com>
2015-12-16 13:52:48 -0800

    Add more quotes tests
    Change-Id: I221e533e575221b21fab8079d80b32302b7e0c61

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QuotesController, type: :controller do
-  let(:quote)    { FactoryGirl.create(:quote) }
+  let(:quote)    { create(:quote) }
 
   describe "#new" do 
     it "renders the new template" do
@@ -15,4 +15,34 @@ RSpec.describe QuotesController, type: :controller do
     end
   end
 
+  describe "#create" do
+    context "with valid parameters" do
+      def valid_request
+        post :create, quote: attributes_for(:quote)
+      end
+      
+      it "changes the quotes count by +1" do
+        expect { valid_request }.to change { Quote.count }.by(1)
+      end
+
+      it "redirects to the show page" do
+        valid_request
+        expect(response).to redirect_to quote_path(Quote.last)
+      end
+    end
+    context "with invalid parameters" do
+      def invalid_request
+        post :create, quote: attributes_for(:quote).merge({body: nil})
+      end
+
+      it "renders the new template filled" do
+        invalid_request
+        expect(response).to render_template(:new)
+      end
+
+      it "doesn't change the quotes count" do
+        expect { invalid_request }.to_not change { Quote.count }
+      end
+    end
+  end
 end
