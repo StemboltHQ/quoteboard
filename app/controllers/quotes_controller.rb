030dfb14d9a58fef9e45282c7c686b94f16310b1
Murphy Stroppa <Murph33@gmail.com>
2015-12-16 14:14:54 -0800

    Add quote actions and routes
    Change-Id: I3fb21acb47fbddc6f01788f003ed6761048681fd

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,24 @@
 class QuotesController < ApplicationController
+  def show
+    @quote = Quote.find params[:id]
+  end
+  
+  def new
+    @quote = Quote.new
+  end
+
+  def create 
+    @quote = Quote.new quote_params
+    if @quote.save
+      redirect_to @quote
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:body, :author, :location)
+  end
 end
