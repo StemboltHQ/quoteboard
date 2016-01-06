b1e6ba9417c13df33648495135ba44ce3bce19b4
Murphy Stroppa <murphy.stroppa@gmail.com>
2016-01-05 16:35:54 -0800

    Add score method to tally up votes for a quote.
    Also add a test that verifies that the quote.score method will count
    tally votes associated with the quote, but not a vote that is not
    associated with the quote.

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
   let(:quote) { FactoryGirl.create(:quote) }
+  let(:user) { FactoryGirl.create(:user) }
 
   it "requires a body" do
     quote.body = nil
     expect(quote).to be_invalid
+  end
+
+  describe "#score" do
+    before do
+      create(:vote, value: 2, quote: nil)
+      create(:vote, quote: quote, value: 2)
+      create(:vote, quote: quote, value: 2, user: create(:user))
+    end
+
+    it "has a score function that tallies votes" do
+      expect(quote.score).to eq(4)
+    end
   end
 end
