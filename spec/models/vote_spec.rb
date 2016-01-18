06503ff888d0246794932488a8173fd9b8aca836
Murphy Stroppa <murphy.stroppa@gmail.com>
2016-01-18 15:31:12 -0800

    Make vote validation concise
    The shoulda gem has been implemented.  Use provided syntax to shorten
    the vote value validation to a one-liner.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  let(:vote) { create(:vote) }
-  describe "validation" do
-    it 'requires a value' do
-      vote.value = nil
-      expect(vote).to be_invalid
-    end
-  end
+  it { is_expected.to validate_presence_of(:value) }
 end
