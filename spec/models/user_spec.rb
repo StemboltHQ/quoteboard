d562677bb823094e4d34eb3a2d3f82b74e59f41e
Murphy Stroppa <murphy.stroppa@gmail.com>
2016-01-18 15:55:39 -0800

    Convert user model test to shoudla syntax.
    The test on the user model spec for the validation of the email presence
    is replaced to use the shoulda gem syntax so it's now a one-liner.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { create(:user) }
-
   describe 'validations' do
-    it 'requires an email' do
-      user.email = nil
-      expect(user).to be_invalid
-    end
+    it { is_expected.to validate_presence_of(:email) }
   end
 end
