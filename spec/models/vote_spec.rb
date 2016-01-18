ba70ce053544d1717febbfa84c88e4f2421979e6
Murphy Stroppa <murphy.stroppa@gmail.com>
2016-01-18 15:51:52 -0800

    Validate uniqueness of vote: scoped to a quote
    A user may vote on any quote, but only once and this is a test for that
    validation.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  it { is_expected.to validate_presence_of(:value) }
+  let(:user) { create(:user) }
+  let(:vote) { create(:vote, user: user) }
+  describe "validations" do
+    subject { vote }
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:quote_id) }
+    it { is_expected.to validate_presence_of(:value) }
+  end
 end
