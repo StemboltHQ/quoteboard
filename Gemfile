b68617509ad25a3c304d55e25194645a60c82c29
Murphy Stroppa <murphy.stroppa@gmail.com>
2016-01-20 11:19:53 -0800

    Add select2 gem
    The select2 gem will be utilized to give an autocomplete when entering a
    user.  We want to just be quoting people in house so will have an
    enumerable or something of the people in house to be used as quoteable
    people which will populate the select box.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'simple_form'
+gem "select2-rails"
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
+//= require select2
 //= require_tree .
