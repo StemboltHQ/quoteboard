32752a92ed62d410b49ce5bbaa9a6b3c04e809c8
Murphy Stroppa <murphy.stroppa@gmail.com>
2016-01-21 15:43:40 -0800

    Add a view based on score
    View sorts all quotes based on their current score.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root "quotes#index"
   resources :quotes do
+    get :best, on: :collection
     resources :votes, only: [:create, :update, :destroy]
   end
 end
